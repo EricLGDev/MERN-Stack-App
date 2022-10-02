fcecd8184b243fb5ac4ab87c13a743988f572250
Eric <ericlg.link@gmail.com>
2022-10-02 03:10:03 -0700

    simple middleware implemented to log requests in terminal

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,12 @@ const express = require('express')
 //express app
 const app = express()
 
+//middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
 //routes
 app.get('/', (req, res) => {
     res.json({mssg: 'Welcome to the app'})
@@ -12,5 +18,5 @@ app.get('/', (req, res) => {
 
 //listen for request
 app.listen(process.env.PORT, () => {
-    console.log('Listening on Port 4000')
+    console.log('Listening on port', process.env.PORT)
 })
