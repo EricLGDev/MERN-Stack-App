a9e7fc8ccb2ff8d821271ef3aa376aefd1457cf1
Eric <ericlg.link@gmail.com>
2022-10-28 18:43:31 -0700

    Establish necessary routes for app

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,7 +1,28 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', () => {})
+router.get('/', (req, res) => {
+    res.json({mssg: 'GET all workouts'})
+})
 
+//GET a single workout
+router.get('/:id', (req, res) => {
+    res.json({mssg: 'GET single workout'})
+})
 
-module.exports = routes
+//POST new workout
+router.post('/', (req, res) => {
+    res.json({mssg: 'POST a new workout'})
+})
+
+//DELETE a workout
+router.post('/:id', (req, res) => {
+    res.json({mssg: 'DELETE a new workout'})
+})
+
+//UPDATE a workout
+router.patch('/:id', (req, res) => {
+    res.json({mssg: 'UPDATE a new workout'})
+})
+
+module.exports = router
