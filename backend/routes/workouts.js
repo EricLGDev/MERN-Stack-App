7f0aece8353261cb32c631cbcbbdac47dc56d436
Eric <ericlg.link@gmail.com>
2022-10-28 18:56:36 -0700

    Postman testing successful

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -5,22 +5,22 @@ router.get('/', (req, res) => {
     res.json({mssg: 'GET all workouts'})
 })
 
-//GET a single workout
+//GET a single workout | Postman Testing = Success 
 router.get('/:id', (req, res) => {
     res.json({mssg: 'GET single workout'})
 })
 
-//POST new workout
+//POST new workout | Postman Testing = Success
 router.post('/', (req, res) => {
     res.json({mssg: 'POST a new workout'})
 })
 
-//DELETE a workout
+//DELETE a workout | Postman Testing = Success
 router.post('/:id', (req, res) => {
     res.json({mssg: 'DELETE a new workout'})
 })
 
-//UPDATE a workout
+//UPDATE a workout | Postman Testing = Success
 router.patch('/:id', (req, res) => {
     res.json({mssg: 'UPDATE a new workout'})
 })
